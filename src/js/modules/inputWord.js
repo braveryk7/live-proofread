97bbce0d9a96a342a16663130a3d883e017c14c2
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-07-04 01:09:07 +0900

    コードの可視性修正

--- src/js/modules/inputWord.js
+++ src/js/modules/inputWord.js
@@ -1,7 +1,7 @@
 import { h } from './h.js';
 import { counter } from './counter.js';
-import { readingPoint } from './readingPoint.js';
 import { characterWidthCheck } from './characterWidthCheck.js';
+import { readingPoint } from './readingPoint.js';
 
 export function inputWord(inputWordResult) {
     /*
@@ -12,30 +12,33 @@ export function inputWord(inputWordResult) {
     */
     const inputWordResultLines = inputWordResult.value.replace(/\r\n|\r/g, '\n').split('\n');    
     let inputWordResultLine = [];
-    let lineEnd = /([。！？）!?)])/
+    const lineEnd = /([。！？）!?)])/
 
     for(let i = 0; i < inputWordResultLines.length; i++) {
         let multiFlag = 0;
         if(counter(inputWordResultLines[i], '。') >= 2) {
-            inputWordResultLine.push(h(inputWordResultLines[i]).replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').split(lineEnd));
+            const htmlReplace = h(inputWordResultLines[i]).replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'');
+            inputWordResultLine.push(htmlReplace.split(lineEnd));
 
             if(inputWordResultLine[inputWordResultLine.length - 1].slice(-1) !== lineEnd); {
                 multiFlag = 1;
             }
             for(let i2 = 0; i2 < inputWordResultLine[i].length; i2++) {
                 if(multiFlag === 0 || (multiFlag === 1 && i2 < inputWordResultLine[i].length - 1)) {
-                    inputWordResultLine[i][i2] = characterWidthCheck(inputWordResultLine[i][i2]);
+                    const inputWordCWC = characterWidthCheck(inputWordResultLine[i][i2]);
+                    inputWordResultLine[i][i2] = inputWordCWC;
                 } else if(multiFlag === 1 && i2 !== inputWordResultLine[i].length - 1) {
-                    inputWordResultLine[i][i2] = characterWidthCheck(inputWordResultLine[i][i2]);
+                    const inputWordCWC = characterWidthCheck(inputWordResultLine[i][i2]);
+                    inputWordResultLine[i][i2] = inputWordCWC;
                 }
             }
         } else {
-            inputWordResultLine.push(characterWidthCheck(h(inputWordResultLines[i]).replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, '')))
+            const htmlReplace = h(inputWordResultLines[i].replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, ''));
+            const inputWordCWC = characterWidthCheck(htmlReplace);
+            inputWordResultLine.push(inputWordCWC);
         }
         multiFlag = 0;
     }
-
-    // console.log(inputWordResultLine);
 
     // 読点の処理
     readingPoint(inputWordResultLine);
