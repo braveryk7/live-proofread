ba1f18aff1cec43a169fb5a350fba5145d90ae04
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-07-05 10:58:51 +0900

    forループをmapメソッド化

--- src/js/modules/copyToClipboard.js
+++ src/js/modules/copyToClipboard.js
@@ -11,14 +11,25 @@ export function copyToClipboard(e) {
     //クリップボードに書き込み
     let text = this.value.value.replace(/\r\n|\r/g, '\n').split('\n');
     const copyArray = [];
-    for (let i = 0; i < text.length; i++) {
-        if(lineFeedCodeChecked !== 'br' && lineFeedCodeChecked !== 'p' ) {
-            copyArray.push(`${text[i]}\n`);
-        } else if(lineFeedCodeChecked === 'br') {
-            copyArray.push(`${text[i]}<br />\n`);
-        } else {
-            copyArray.push(`<${lineFeedCodeChecked}>${text[i]}</${lineFeedCodeChecked}>\n`)
-        }
+    if(lineFeedCodeChecked !== 'br' && lineFeedCodeChecked !== 'p' ) {
+        text.map( (value) => copyArray.push(`${value}\n`));
+        console.log('コピーしたよ')
+    } else if(lineFeedCodeChecked === 'br') {
+        text.map( (value) => copyArray.push(`${value}<br />\n`));
+        console.log('コピーしたぜ')
+    } else {
+        text.map( (value) => copyArray.push(`<p>${value}</p>\n`));
+        console.log('コピーした')
     }
+    
+    // for (let i = 0; i < text.length; i++) {
+    //     if(lineFeedCodeChecked !== 'br' && lineFeedCodeChecked !== 'p' ) {
+    //         copyArray.push(`${text[i]}\n`);
+    //     } else if(lineFeedCodeChecked === 'br') {
+    //         copyArray.push(`${text[i]}<br />\n`);
+    //     } else {
+    //         copyArray.push(`<${lineFeedCodeChecked}>${text[i]}</${lineFeedCodeChecked}>\n`)
+    //     }
+    // }
     navigator.clipboard.writeText(copyArray.join('').slice(0, -1));
 }
