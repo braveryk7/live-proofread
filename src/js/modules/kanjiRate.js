4440ec14356cb3ea1407c42e638ea604b29c0fc0
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-07-07 01:20:06 +0900

    #5 漢字利用率ロジック実装

---
+++ src/js/modules/kanjiRate.js
@@ -0,0 +1,15 @@
+export function kanjiRate(str) {
+    const kanjiCheckReg = /([\u3400-\u9FFF])/;
+    let total = 0;
+    let kanji = 0;
+
+    const strChara = Array.from(str).map((chara) => {
+        if(kanjiCheckReg.test(chara)) {
+            total++;
+            kanji++;
+        } else {
+            total++;
+        }
+    });
+    return (kanji / total * 100).toFixed(1);
+}
