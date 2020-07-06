71971d2bb042ab5376968817ed5da51c3e916df0
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-07-06 15:09:08 +0900

    ES6にリファクタリング

--- src/js/modules/readingPoint.js
+++ src/js/modules/readingPoint.js
@@ -1,19 +1,21 @@
 import { counter } from './counter.js';
 
-export function readingPoint(inputWordResultLine) {
-    //読点の処理
-    if(document.getElementById('reading_point_checkbox').checked === true) {
-        for(let i = 0; i < inputWordResultLine.length; i++) {
-            if(Array.isArray(inputWordResultLine[i]) === true) {
-                for(let i2 = 0; i2 < inputWordResultLine[i].length; i2++) {
-                    if(counter(inputWordResultLine[i][i2], '、') > document.getElementById('reading_point_number').value) {
-                        inputWordResultLine[i][i2] = `<span class="view-word--underline" title="読点（、）の数を減らして下さい">${inputWordResultLine[i][i2]}</span>`;
-                    }
+export function readingPoint(arr) {
+    let readingPointResult = arr.map((line) => {
+        if (Array.isArray(line)) {
+            let lines = line.map((doubleLine) => {
+                if(counter(doubleLine, '、') > document.getElementById('reading_point_number').value) {
+                    return `<span class="view-word--underline" title="読点（、）の数を減らして下さい">${doubleLine}</span>`;
+                } else {
+                    return doubleLine;
                 }
-                inputWordResultLine[i] = inputWordResultLine[i].join('');
-            } else if(counter(inputWordResultLine[i], '、') > document.getElementById('reading_point_number').value) {
-                inputWordResultLine[i] = `<span class="view-word--underline" title="読点（、）の数を減らして下さい">${inputWordResultLine[i]}</span>`;
-            }
+            })
+            return lines;
+        } else if (counter(line, '、') > document.getElementById('reading_point_number').value) {
+                return `<span class="view-word--underline" title="読点（、）の数を減らして下さい">${line}</span>`;
+        } else {
+            return line;
         }
-    }
+    })
+    return readingPointResult;
 }
