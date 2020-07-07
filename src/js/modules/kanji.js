0f6c17541deb671a4dc85a10f799edeb63efcc4d
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-07-08 00:58:55 +0900

    #2 低可読性漢字の指摘ロジック実装

---
+++ src/js/modules/kanji.js
@@ -0,0 +1,25 @@
+import { h } from './h.js';
+
+export function kanji(arr) {
+    const checkReg = new RegExp(`(${h(document.getElementById('kanjiValue').value.replace(',', '|').replace(/\s+/g, ''))})`);
+    const htmlTagReg = /(<[^>]+>)/;
+
+    const kanjiArray = arr.map((line) => {
+        const htmlCheck = line.split(htmlTagReg);
+        const kanjiLine = htmlCheck.map((kanjiValue) => {
+            return htmlTagReg.test(kanjiValue) ? kanjiValue : kanjiValue.split(checkReg);
+        })        
+        const kanjiCheck = kanjiLine.map((kanjiValue) => {
+            if(Array.isArray(kanjiValue)){
+                const kanjiList = kanjiValue.map((kanjiListValue) => {
+                    return checkReg.test(kanjiListValue) ? `<span class="view-word--kanji-check" title="ひらがなに直して下さい">${kanjiListValue}</span>` : kanjiListValue;
+                })
+                return kanjiList.join('');
+            } else {
+                return kanjiValue;
+            }
+        })
+        return kanjiCheck.join('')
+    })
+    return kanjiArray;
+}
