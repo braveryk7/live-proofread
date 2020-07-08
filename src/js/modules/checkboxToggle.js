2a05eb7852bc6e9debc1d34e6c6195df258718fa
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-07-08 12:44:17 +0900

    #9 typo修正

--- src/js/modules/checkboxToggle.js
+++ src/js/modules/checkboxToggle.js
@@ -2,8 +2,8 @@ export function checkboxToggle(id, targetId) {
     const target = document.getElementById(targetId);
 
     if(document.getElementById(id).checked === true) {
-        target.disabled === false;
+        target.disabled = false;
     } else {
-        target.disabled === true;
+        target.disabled = true;
     }
 }
