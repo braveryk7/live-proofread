a92f434f0c0e2e799c8ce659de381ca2994d21bb
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2020-07-14 19:36:58 +0900

    #65 設定値取得をモジュール切り出し

--- src/js/modules/jsonDownload.js
+++ src/js/modules/jsonDownload.js
@@ -1,16 +1,8 @@
-export function jsonDownload() {   
-    let rpc, cw, hm, kj, kv = '';
+import { valueAcquisition } from './valueAcquisition.js'
 
-    if(document.getElementById('character_width_none').checked) { cw = 0; }
-    else if(document.getElementById('character_width_half').checked) { cw = 1; }
-    else if(document.getElementById('character_width_full').checked) { cw = 2; }
+export function jsonDownload() {
+    const jsonData = valueAcquisition();
 
-    document.getElementById('reading_point_checkbox').checked === true ? rpc = 1 : rpc = 0;
-    document.getElementById('heatmap').checked === true ? hm = 1 : hm = 0;
-    document.getElementById('kanjiCheckBox').checked === true ? kj = 1 : kj = 0;
-    document.getElementById('kanjiValue').value && kj === 1 ? kv = document.getElementById('kanjiValue').value : kv = '';
-
-    const jsonData = { rpc, cw, hm, kj, kv }
     const jsonBlob = new Blob(
         [JSON.stringify(jsonData, null, '    ')],
         { type: 'application/json' }
